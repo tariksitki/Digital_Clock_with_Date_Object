35d25750119430fc6d2e8e0dc5eaa0cbf99737a7
tarik <63872091+tariksitki@users.noreply.github.com>
2022-03-12 09:27:17 +0100

    digital clock completed

---
+++ App.js
@@ -0,0 +1,19 @@
+
+let hour = document.getElementById("hour");
+let minute = document.getElementById("minute");
+let second = document.getElementById("second");
+
+
+function updateTime() {
+    let currentTime = new Date();
+    let currentHour = currentTime.getHours();
+    let currentMinute = currentTime.getMinutes();
+    let currentSecond = currentTime.getSeconds();
+
+    hour.innerText = currentHour.toString().padStart(2, 0);
+    minute.innerText = currentMinute.toString().padStart(2, 0);
+    second.innerText = currentSecond.toString().padStart(2, 0);
+
+}
+
+setInterval(updateTime, 1000);
